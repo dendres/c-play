434ee44810d1398f7c6c4560a7b78072446976ec
Don Endres <don@endres.us>
2018-01-07 10:25:50 -0500

    compared kr1-20 with others

--- kr/detab.c
+++ kr/detab.c
@@ -6,6 +6,17 @@ replace tabs with spaces
 */
 
 # define MAXLINE 1000
+# define SPACE ' '
+# define TAB '\t'
+# define NULLC '\0'
+# define TABSTEP 8
+
+void copy(char to[], char from[]){
+        int i;
+        i = 0;
+         while( (to[i] = from[i]) != '\0')
+                ++i;
+}
 
 
 int get_line(char s[], int lim) {
@@ -22,41 +33,51 @@ int get_line(char s[], int lim) {
 }
 
 
-int main() {
-        int i, j, l, len;
-        char line[MAXLINE];
+// takes a \0 terminated character array
+// and modifies it by replacing tab characters with spaces
+void detab_line(char line[]) {
+        int i, j, k, spaces;
         char out[MAXLINE];
-        // tabs have to remain in columns every 8 spaces from the beginning of the line
-        // so can't look for just any 8 spaces...
-        // must look for multiples of 8 starting from beginning of the line
 
-        // had to cheat to get the number of spaces to add = (tab - (i % tab))
-
-        // get a line.
-        while ((len = get_line(line, MAXLINE)) > 0) {
-
-                i = 0;
-                j = 0;
-                while (line[i] != '\0') {
-                        if (line[i] == '\t') {
-                                out[j] = ' ';
-                                l = j + (8 - (j % 8) ) - 1;
-                                while (j < l) {
-                                        out[++j] = ' ';
-                                }
-                        } else {
-                                out[j] = line[i];
+        for (i = 0, j = 0; line[i] != NULLC; ++i, ++j) {
+                if (line[i] == TAB) {
+                        spaces = (TABSTEP - (j % TABSTEP) ); // cheated here
+                        for(k = j; j < (k + spaces); ++j) {
+                                out[j] = SPACE;
                         }
-
-                        ++i;
-                        ++j;
+                        --j; // have to back up one step after writing all the spaces?????
+                } else {
+                        out[j] = line[i];
                 }
+        }
+        out[j] = NULLC;
+        copy(line, out);
+}
 
-                out[j] = '\0';
 
-                printf("%s", out);
+int main() {
+        int len;
+        char line[MAXLINE];
+
+        while ((len = get_line(line, MAXLINE)) > 0) {
+                detab_line(line);
+                printf("%s", line);
         }
 
         return 0;
 }
+
+/*
+test:
+these 2 should produce identical spacing:
+
+echo -e "1\t234\t567\t89"
+echo -e "1\t234\t567\t89" | ./detab
+
+
+compare to others:
+ - SPACE and TAB as constants
+ - comma separated initial conditions in for loop: for(i=0,j=0,k=0)
+   and ++j,++j,++k in for loop
+*/
 
